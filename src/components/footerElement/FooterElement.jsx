69c1eaea441b64efc779ea5fc7f81e23bef54526
sobirovmurodil <murodil@gmail.com>
2024-06-09 14:20:20 +0500

    fixed

--- src/components/footerElement/FooterElement.jsx
+++ src/components/footerElement/FooterElement.jsx
@@ -9,9 +9,9 @@ function FooterElement() {
           <h2 className='footer-element-container-left-title'>
             Bringing you the <span className='best'>best</span> audio gear
           </h2>
-          <p className='footer-element-container-left-text body-text'>
+          <small className='footer-element-container-left-text body-text'>
             Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.
-          </p>
+          </small>
         </div>
         <div>
           <img className='hero-img' src={heroImg} alt="" />
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Navbar } from './navbar/Navbar';
 export { default as Footer } from './footer/Footer';
 export { default as ErrorElement } from './errorElement/ErrorElemet';
-export { default as FooterElement } from './footerElement/footerElement';
+export { default as FooterElement } from './footerElement/FooterElement';
 export { default as Menu } from './menu/Menu';
